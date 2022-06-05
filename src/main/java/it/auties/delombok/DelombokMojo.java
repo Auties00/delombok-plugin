32ebffee74b066ae23984c592b627dadaf8bd858
Auties00 <alautiero@gmail.com>
2022-06-05 19:06:28 +0200

    Updated pom

--- src/main/java/it/auties/delombok/DelombokMojo.java
+++ src/main/java/it/auties/delombok/DelombokMojo.java
@@ -7,27 +7,38 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toUnmodifiableList;
+
 @Mojo(name = "delombok")
 public class DelombokMojo extends AbstractMojo {
-    private static final String JAVA_COMMAND = "java -jar %s delombok %s -d %s";
-
     @Parameter(required = true)
     private File rootDirectory, outputDirectory;
 
     @Parameter
     private final Set<String> excludedFiles = Set.of("module-info.java");
 
+    @Parameter
+    private final Map<String, String> parameters = Map.of();
+
     @SneakyThrows
     @Override
     public void execute() {
+        if(rootDirectory.exists()){
+            getLog().error("Root directory doesn't exist");
+            return;
+        }
+
         var start = System.currentTimeMillis();
         getLog().info("Starting delombok process...");
 
@@ -35,23 +46,36 @@ public class DelombokMojo extends AbstractMojo {
         getLog().info(String.format("Detected %s files", sources.size()));
 
         var transform = generateDelombokInput(sources);
-        runDelombok(transform);
+        if(!runDelombok(transform)){
+            return;
+        }
 
-        moveOutputToCorrectDir(sources);
+        fixPath(sources);
         getLog().info(String.format("Finished delombok process, took %s ms", System.currentTimeMillis() - start));
     }
 
     @SneakyThrows
-    private void moveOutputToCorrectDir(List<Path> sources) {
-        Files.walk(outputDirectory.toPath())
-                .filter(Files::isRegularFile)
-                .forEach(path -> {
-                    var fileName = path.getFileName().toString();
-                    var outputFileName = findMatchingSourceFile(sources, fileName);
-                    var outputPath = Path.of(outputDirectory.getPath(), outputFileName);
-                    createDirectories(outputPath);
-                    moveFile(path, outputPath);
-                });
+    private List<Path> findJavaSources() {
+        try(var walker = Files.walk(rootDirectory.toPath())){
+            return walker.filter(Files::isRegularFile)
+                    .collect(toUnmodifiableList());
+        }
+    }
+
+    @SneakyThrows
+    private void fixPath(List<Path> sources) {
+        try(var walker = Files.walk(outputDirectory.toPath())){
+            walker.filter(Files::isRegularFile)
+                    .forEach(path -> fixPath(sources, path));
+        }
+    }
+
+    private void fixPath(List<Path> sources, Path path) {
+        var fileName = path.getFileName().toString();
+        var outputFileName = findMatchingSourceFile(sources, fileName);
+        var outputPath = Path.of(outputDirectory.getPath(), outputFileName);
+        createDirectories(outputPath);
+        moveFile(path, outputPath);
     }
 
     private String findMatchingSourceFile(List<Path> sources, String fileName) {
@@ -65,30 +89,51 @@ public class DelombokMojo extends AbstractMojo {
 
     private String generateDelombokInput(List<Path> sources) {
         return sources.stream()
-                .filter(this::filterPath)
+                .filter(this::shouldDelombok)
                 .map(Path::toString)
                 .collect(Collectors.joining(" "));
     }
 
     @SneakyThrows
-    private void runDelombok(String transform) {
-        new ProcessBuilder(String.format(JAVA_COMMAND, Path.of(Lombok.class.getProtectionDomain().getCodeSource().getLocation().toURI()), transform, outputDirectory.getPath()).split(" "))
-                .redirectError(ProcessBuilder.Redirect.DISCARD)
-                .start()
-                .waitFor();
+    private boolean runDelombok(String input) {
+        var lombok = findLombokJar();
+        var output = outputDirectory.toPath().toAbsolutePath();
+        var command = String.format("java -jar %s delombok %s -d %s %s",
+                lombok, input, output, createParameters());
+        var process = Runtime.getRuntime().exec(command);
+        if(process.waitFor() != 0){
+            var error = new String(process.getErrorStream().readAllBytes(), StandardCharsets.UTF_8);
+            getLog().error(String.format("Cannot delombok sources: %s", error));
+            return false;
+        }
+
+        var info = new String(process.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+        getLog().info(String.format("Delombok output: %s", info));
+        return true;
+    }
+
+    private String createParameters() {
+        return parameters.entrySet()
+                .stream()
+                .map(DelombokMojo::createParameter)
+                .collect(Collectors.joining(" "));
+    }
+
+    private static String createParameter(Entry<String, String> entry) {
+        var key = entry.getKey();
+        var value = entry.getValue() == null ? ""
+                : String.format("=%s", entry.getValue());
+        return String.format("--%s%s", key, value);
     }
 
     @SneakyThrows
-    private List<Path> findJavaSources() {
-        return Files
-                .walk(rootDirectory.toPath())
-                .filter(Files::isRegularFile)
-                .collect(Collectors.toList());
-    }
-
-    @SneakyThrows
-    private void createDirectories(Path outputPath) {
-        Files.createDirectories(outputPath.getParent());
+    private static Path findLombokJar() {
+        var lombok = Lombok.class.getProtectionDomain()
+                .getCodeSource()
+                .getLocation()
+                .toURI();
+        return Path.of(lombok)
+                .toRealPath();
     }
 
     @SneakyThrows
@@ -97,14 +142,21 @@ public class DelombokMojo extends AbstractMojo {
     }
 
     @SneakyThrows
-    private boolean filterPath(Path path) {
-        if(excludedFiles.stream().anyMatch(path.getFileName().toString()::equals)){
-            var outputPath = Path.of(outputDirectory.getPath(), path.getFileName().toString());
-            createDirectories(outputPath);
-            Files.write(outputPath, Files.readAllBytes(path), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
-            return false;
+    private boolean shouldDelombok(Path input) {
+        if (excludedFiles.stream().noneMatch(input.getFileName().toString()::equals)) {
+            return true;
         }
 
-        return true;
+        var output = Path.of(outputDirectory.getPath(),
+                input.getFileName().toString());
+        createDirectories(output);
+        Files.write(output, Files.readAllBytes(input),
+                StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        return false;
+    }
+
+    @SneakyThrows
+    private void createDirectories(Path outputPath) {
+        Files.createDirectories(outputPath.getParent());
     }
 }
